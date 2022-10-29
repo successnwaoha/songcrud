329f491e71d95cb91cbc5cfea534b093caf01656
Success Nwaoha <nwaohasuccess2306@gmail.com>
2022-10-29 09:46:16 +0100

    django task

--- musicapp/models.py
+++ musicapp/models.py
@@ -1,3 +1,19 @@
+from pyexpat import model
 from django.db import models
 
 # Create your models here.
+
+class Artiste(models, model):
+    first_name = models.CharField()
+    last_name = models.CharField()
+    age = models.IntegerField()
+
+class Song(models, model):
+    title = models.CharField()
+    date_released = models.IntegerField()
+    likes = models.CharField()
+    artiste_id = models.IntegerField()
+
+class Lyrics(models, model):
+    content = models.CharField()
+    song_id = models.IntegerField()
